aedd6573a455de5416f43d7c4aa8c46f9e56d515
unknown <sjjoseph106@gmail.com>
2022-06-22 17:56:34 +0900

    :sparkles: 구글 소셜 로그인 추가

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "../fbase";
+import { authService, firebaseInstance } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -34,6 +34,11 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async () => {
+    let provider = new firebaseInstance.auth.GoogleAuthProvider();
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
 
   return (
     <div>
@@ -64,7 +69,7 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
+        <button onClick={onSocialClick}>Continue with Google</button>
       </div>
     </div>
   );
